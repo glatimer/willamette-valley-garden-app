9a2f604c5641e96e8d7024d56c21d0af60f52ca9
Genevieve Latimer and Heather McCarthy <newwavehealing@gmail.com>
2016-04-04 20:16:49 -0700

    Create produce constructor and tomato object - functioning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // business logic
-
+var tomato = new Produce("tomato", "full-sunshine", "3 feet apart", "1/4 inch", "7-14 days", "80 days from seed")
+function Produce (item, sunshine, spacing, seedDepth, germination, harvest) {
+  this.item = item,
+  this.sunshine = sunshine,
+  this.spacing = spacing,
+  this.seedDepth = seedDepth,
+  this.germination = germination,
+  this.harvest = harvest,
+};
 
 
 
