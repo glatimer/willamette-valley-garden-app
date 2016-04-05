143a5b93f8e217857d557487d83d99e2a87194f4
Sarah Read-Brown <sreadbrown@gmail.com>
2016-04-04 20:56:04 -0700

    moved functions to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 // business logic
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
 
 $(document).ready(function() {
 
+  // $("draggable").draggable();
 
 
 
