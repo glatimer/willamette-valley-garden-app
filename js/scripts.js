12b10c622667d3a2d10fff47aa8e7b96541b182e
Priya Vasudevan <satipriya99@gmail.com>
2016-04-13 19:44:27 -0700

    add uppercase for the veggies.

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,21 @@ function Produce (divid, item, sunshine, spacing, seedDepth, germination, harves
   this.harvest = harvest
 };
 
-var carrot = new Produce("carrot", "carrot", "carrot-sunshine", "carrot feet apart", "1/4 carrot inches", "7-14 carrot", "80 carrot from seed")
+var carrot = new Produce("carrot", "Carrot", "carrot-sunshine", "carrot feet apart", "1/4 carrot inches", "7-14 carrot", "80 carrot from seed")
 
-var tomato = new Produce("tomato", "tomato", "full-sunshine", "3 feet apart", "1/4 inch", "7-14 days", "80 days from seed")
+var tomato = new Produce("tomato", "Tomato", "full-sunshine", "3 feet apart", "1/4 inch", "7-14 days", "80 days from seed")
 
-var basil = new Produce("basil", "basil", "full-sunshine", "2 inches apart", "1/4 inch", "5-10 days", "60 days from seed")
+var basil = new Produce("basil", "Basil", "full-sunshine", "2 inches apart", "1/4 inch", "5-10 days", "60 days from seed")
 
-var beetroot = new Produce ("betroot", "beetroot", "part-sunshine", "12 inches apart", "1/2 inch", "10-14 days", "90 days from seed")
+var beetroot = new Produce ("betroot", "Beetroot", "part-sunshine", "12 inches apart", "1/2 inch", "10-14 days", "90 days from seed")
 
-var cabbage = new Produce ("cabbage", "cabbage", "light-shade-sunshine", "18 inches apart", "1/2 inch", "10-14 days", "90-100 days from seed")
+var cabbage = new Produce ("cabbage", "Cabbage", "light-shade-sunshine", "18 inches apart", "1/2 inch", "10-14 days", "90-100 days from seed")
 
-var carrot = new Produce ("carrot", "carrot", "full-sunshine", "12 inches apart", "1/2 inch", "14-21 days", "68 days from seed")
+var dill = new Produce ("dill", "Dill", "shade", "12 inches apart", "1/2 inch", "7-10 days", "90 days from seed")
 
-var dill = new Produce ("dill", "dill", "shade", "12 inches apart", "1/2 inch", "7-10 days", "90 days from seed")
+var eggplant = new Produce ("eggplant", "Eggplant", "full-sunshine", "12 inches apart", "1/2 inch", "7-14 days", "120 days from seed")
 
-var eggplant = new Produce ("eggplant", "eggplant", "full-sunshine", "12 inches apart", "1/2 inch", "7-14 days", "120 days from seed")
-
-var lavender = new Produce ("lavender", "lavender", "indoor-light sunshine", "1 inch apart", "1/4 inch", "18-28 days", "90- 100 days from seed")
+var lavender = new Produce ("lavender", "Lavender", "indoor-light sunshine", "1 inch apart", "1/4 inch", "18-28 days", "90- 100 days from seed")
 
 Produce.prototype.information = function() {
   return "<div id='" + this.divid + "'> <li>Name: " +this.item + "</li> <li>Sun required: " + this.sunshine + "</li> <li>Spacing: " + this.spacing + "</li> <li>Seed Depth: " + this.seedDepth + "</li> <li>Germination: " + this.germination + "</li> <li>Harvest: " + this.harvest +"</li><br></div>";
