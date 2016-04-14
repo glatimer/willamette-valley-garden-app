7dd47c0b76a26620a7256f3641768c2772dc479c
Genevieve Latimer <genevievelatimer@gmail.com>
2016-04-13 20:07:06 -0700

    changed raddish spelling

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,13 @@ var peas = new Produce("peas", "Sugar Snap Peas", "full-sunshine", "2-3 inches",
 
 var pepper = new Produce("pepper", "Red Bell Pepper", "full-sunshine", "1 inch", "1/4 inch", "10-21 days", "74-76 days from transplant")
 
-var radish = new Produce("radish", "Radish", "full-sunshine", "1 inch", "1/2 inch", "5-7 days", "50-60 days from seed")
+var raddish = new Produce("radish", "Radish", "full-sunshine", "1 inch", "1/2 inch", "5-7 days", "50-60 days from seed")
 
 var rosemary = new Produce("rosemary", "Rosemary", "full-sunshine", "2 inches", "5-10 days", "66 days from seed")
 
 var thyme = new Produce("thyme", "Thyme", "full-sunshine", "1 inch", "1/4 inch", "14-28 days", "85 days from seed")
 
-var gardenItems = [carrot, tomato, basil, parsley, peas, pepper, radish, rosemary]
+var gardenItems = [carrot, tomato, basil, parsley, peas, pepper, raddish, rosemary]
 
 
 Produce.prototype.information = function() {
